fd0bbb03b5c47aa6e3fc9b370eaf43df78471dd1
Nikita <Libino123@yandex.ru>
2023-03-18 20:20:05 +0600

    1* task added

---
+++ 1*.py
@@ -0,0 +1,21 @@
+# *** (1)У вас есть массив чисел, составьте из них максимальное число. Например:
+# [61, 228, 9] -> 961228
+
+list = [61, 228, 9]
+
+for i in range(0, len(list)):
+    list[i] = str(list[i])
+print(list)
+
+for j in range(0, len(list)-1):
+    for x in range(1, len(list)):
+        if int(list[j][0]) < int(list[x][0]):
+            temp = list[j]
+            list[j] = list[x]
+            list[x] = temp
+print(list)
+
+result = ""
+for k in range(0, len(list)):
+    result = result + list[k]
+print(result)
